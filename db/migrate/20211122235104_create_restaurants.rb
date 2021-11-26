5886fb0463e9277b0d03fbaf6db145d0b38d79f0
David <daved3128@aol.com>
2021-11-26 16:29:31 -0500

    a

--- db/migrate/20211122235104_create_restaurants.rb
+++ db/migrate/20211122235104_create_restaurants.rb
@@ -5,7 +5,7 @@ class CreateRestaurants < ActiveRecord::Migration[6.1]
       t.string :location, null: false
       t.text :description, null: false
       t.string :photo_url, null: false
-      t.string :yelp_id, null: false 
+      t.string :yelp_id
       t.belongs_to :food, null: false 
       t.timestamps
     end
